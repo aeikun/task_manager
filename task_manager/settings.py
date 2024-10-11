fb553af0fe881be7ee6c64832c422f5f7e9a63e1
aeikun <aeibouyhy@gmail.com>
2024-10-11 20:47:02 +0100

    Update settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -39,9 +39,9 @@ if os.getenv('DATABASE_URL'):
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-#STATICFILES_DIRS = [
-#   os.path.join(BASE_DIR, 'staticfiles'),
-#]
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'staticfiles'),
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
