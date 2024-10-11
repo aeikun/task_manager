36ecc573772cab1edafa7c5ae6be722a1aaefd6b
aeikun <aeibouyhy@gmail.com>
2024-10-11 20:10:13 +0100

    Update settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -125,6 +125,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ],
 }
 
