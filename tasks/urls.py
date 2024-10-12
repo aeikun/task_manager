7b63756580fdc32781660eb9c7a1b7973a3c8f1f
aeikun <aeibouyhy@gmail.com>
2024-10-12 15:17:15 +0100

    Update settings

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,20 +2,17 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import TaskViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import index, YourApiView  # Make sure to import YourApiView
+from .views import index, YourApiView
 
 # Create a router and register the TaskViewSet
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet, basename='task')
 
 urlpatterns = [
-    path('', index, name='index'),  # This maps the root URL to the index view
+    path('', index, name='index'),  # Maps the root URL to the index view
     path('api/', YourApiView.as_view(), name='api_endpoint'),  # Your new API endpoint
-    path('api/', include(router.urls)),  # Include the router URLs for tasks
     # JWT authentication endpoints
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    # Uncomment and adjust if you want to add specific task-related paths
-    # path('tasks/', TaskList.as_view(), name='task-list'),  
-    # path('tasks/<int:pk>/', TaskDetail.as_view(), name='task-detail'),
-] + router.urls
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # Token obtain endpoint
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # Token refresh endpoint
+    path('api/', include(router.urls)),  # Include the router URLs for tasks
+]
