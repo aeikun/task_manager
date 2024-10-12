144e135d170f9960a348837e6213a28ddc28a997
aeikun <aeibouyhy@gmail.com>
2024-10-12 15:39:35 +0100

    Update settings

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,7 +1,13 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Task
 
 class TaskSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
+
     class Meta:
         model = Task
         fields = '__all__'
+
+    def get_user(self, obj):
+        return obj.user.username  # Return the username instead of the ID
