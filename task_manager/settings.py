b00a8c7b604b26775a4284097dc9c045bc1cd8f7
aeikun <aeibouyhy@gmail.com>
2024-10-12 14:09:16 +0100

    Update settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Use environment variables for sensitive data
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-default')
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='aeiformanage.herokuapp.com,localhost,127.0.0.1').split(',')
 
