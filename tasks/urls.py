4989f43bd6fded76d5a4f4dbd68285d636184e3f
aeikun <aeibouyhy@gmail.com>
2024-10-12 15:08:38 +0100

    Update settings

--- tasks/urls.py
+++ tasks/urls.py
@@ -11,11 +11,11 @@ router.register(r'tasks', TaskViewSet, basename='task')
 urlpatterns = [
     path('', index, name='index'),  # This maps the root URL to the index view
     path('api/', YourApiView.as_view(), name='api_endpoint'),  # Your new API endpoint
-    path('api/', include(router.urls)),  # Include the router URLs for tasks
     # JWT authentication endpoints
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     # Uncomment and adjust if you want to add specific task-related paths
     # path('tasks/', TaskList.as_view(), name='task-list'),  
     # path('tasks/<int:pk>/', TaskDetail.as_view(), name='task-detail'),
+    path('api/', include(router.urls)),  # Include the router URLs for tasks
 ]
